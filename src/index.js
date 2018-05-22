2f24d8cfdde36fc1b748e2e0776baea75e715593
litvinenkoEvgeny <yourbisnesspartner@gmail.com>
2018-05-22 23:02:18 +0300

    dateinput

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import "./index.pug";
 import "./css/index.sass";
 
+import $ from 'jquery';
+window.jQuery = $;
+
 import customSelect from "custom-select";
-import 'custom-select/build/custom-select.css'
+import 'custom-select/build/custom-select.css';
 
-customSelect('select')
+import flatpickr from "flatpickr";
+import 'flatpickr/dist/flatpickr.min.css';
+import { Russian } from "flatpickr/dist/l10n/ru.js"
 
+customSelect('select');
+const dateInput = document.querySelectorAll('.date-input');
+flatpickr(dateInput, {
+    dateFormat: "Y-m-d H:i",
+    locale: Russian
+});
 
