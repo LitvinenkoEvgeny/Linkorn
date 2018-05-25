5e295a9a786410fc702495a592202450168f5c67
LitvinenkoEvgenyWebDev <yourbisnesspartner@gmail.com>
2018-05-25 18:20:12 +0300

    add components: button, banner, select, checkbox etc.

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 import "./index.pug";
 import "./css/index.sass";
 
-import $ from 'jquery';
+import $ from "jquery";
 window.jQuery = $;
 
 import customSelect from "custom-select";
-import 'custom-select/build/custom-select.css';
+import "custom-select/build/custom-select.css";
 
 import flatpickr from "flatpickr";
-import 'flatpickr/dist/flatpickr.min.css';
-import { Russian } from "flatpickr/dist/l10n/ru.js"
+import "flatpickr/dist/flatpickr.min.css";
+import { Russian, } from "flatpickr/dist/l10n/ru.js";
 
-customSelect('select');
-const dateInput = document.querySelectorAll('.date-input');
+customSelect("select");
+const dateInput = document.querySelectorAll(".date-input");
+
 flatpickr(dateInput, {
     dateFormat: "Y-m-d H:i",
-    locale: Russian
+    locale: Russian,
 });
 
