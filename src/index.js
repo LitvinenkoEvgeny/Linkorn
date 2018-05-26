b2f5ad77631ab847ab80f5a12e9d0494d3dfc58e
LitvinenkoEvgenyWebDev <yourbisnesspartner@gmail.com>
2018-05-26 13:48:32 +0300

    change dates separator in date input, add triangles to select and so on...

--- src/index.js
+++ src/index.js
@@ -5,17 +5,17 @@ import $ from "jquery";
 window.jQuery = $;
 
 import customSelect from "custom-select";
-import "custom-select/build/custom-select.css";
 
 import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css";
-import { Russian, } from "flatpickr/dist/l10n/ru.js";
 
 customSelect("select");
 const dateInput = document.querySelectorAll(".date-input");
 
 flatpickr(dateInput, {
-    dateFormat: "Y-m-d H:i",
-    locale: Russian,
+    mode: "range",
+    dateFormat: "d/M/Y",
+    locale: {
+        rangeSeparator: " - ",
+    },
 });
 
